de3dd566680e56a8e4d5be9908d9c2abb41500fb
ZivaiRuzive <zivaiherbert@gmail.com>
2023-12-13 20:50:10 +0200

    Resolves address into IP

--- remote_host_ip.py
+++ remote_host_ip.py
@@ -1,17 +1,26 @@
 # program file: remote_host_ip
-import socket 
+# ip lookup
 
-import sys 
+import socket 
+import logging 
+import sys
+
+log = logging.getLogger("Remote Host:")
+logging.basicConfig(level=logging.DEBUG)
 
 def get_remote_ip():
-    address = sys.argv[1]
-    try:
-        remote_host = f"IP of {address} is {socket.gethostbyname(address)}"
-        print(remote_host)
-    except socket.error as msg:
-        remote_host = f"{address}:{msg}"
-        print(remote_host)
-    return 
+    if len(sys.argv) >=2:
+        address = sys.argv[1:]
+        for addr in address:
+            try:
+                remote_host = f" IP of {addr} is {socket.gethostbyname(addr)}"
+                log.info(remote_host)
+            except socket.error as msg:
+                remote_host = f"{addr}:{msg}"
+                log.warning(remote_host) 
+    else:
+        log.warning("Provide host address as command line argument")
+        
 
 
 
